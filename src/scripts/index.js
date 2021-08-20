2c74837a0af60c7f3f6889fa471d87da30aed739
j Martin Povis <martin25zxc@gmail.com>
2021-08-19 23:28:04 -0300

    Se agrego al footer un link al perfil de Platzi. Se agregaron estilos tanto para los links como para mejorar la experiencia de cual es mejor. Se mejoro el codigo del index.js para poder resolver las nuevas caracteristicas.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,42 @@
 function calculateBetter(amountCinemas, priceCinemas, arrStreaming) {
   let totalCinemas = amountCinemas * priceCinemas;
   let totalStreaming = arrStreaming.reduce((z, x) => z + x, 0);
-  let result = "";
+
   const formatter = new Intl.NumberFormat("es-AR", {
     style: "currency",
     currency: "ARS",
   });
 
-  if (totalCinemas == totalStreaming)
-    result = `¡Empate! En cuanto a lo económico ambos cuestan ${formatter.format(
-      totalCinemas
-    )}.`;
-  else
-    result = `Te conviene más ${
-      totalCinemas < totalStreaming
-        ? "ir al Cine"
-        : `suscribirte a ${
-            arrStreaming.filter((x) => x != 0).length > 1
-              ? "las plataformas"
-              : "la plataforma"
-          }`
-    }. En total costaría ${formatter.format(
-      Math.min(...[totalStreaming, totalCinemas])
-    )}.`;
+  let min = Math.min(...[totalStreaming, totalCinemas]);
+  let result = {};
+  if (totalCinemas == totalStreaming) {
+    result = {
+      winner: null,
+      amount: min,
+      result: `¡Empate! En cuanto a lo económico ambos cuestan ${formatter.format(
+        min
+      )}.`,
+    };
+  } else if (min == totalCinemas) {
+    result = {
+      winner: "cinema",
+      amount: totalCinemas,
+      result: `Te conviene más ir al Cine. En total costaría ${formatter.format(
+        min
+      )}.`,
+    };
+  } else if (min == totalStreaming) {
+    result = {
+      winner: "streaming",
+      amount: totalStreaming,
+      result: `Te conviene más ${`suscribirte a ${
+        arrStreaming.filter((x) => x != 0).length > 1
+          ? "las plataformas"
+          : "la plataforma"
+      }`}. En total costaría ${formatter.format(min)}.`,
+    };
+  }
+
   return result;
 }
 
